01b7709b4a8da1ba60959af03be4e6ce1191f05d
psai12 <sairampallapothula@gmail.com>
2024-11-28 18:28:53 +0530

    installed_libraries

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express=require('express');
+const dotenv=require('dotenv').config({path:'.env'});
 const app=express();
 
+
+
+app.use(express.urlencoded({extended:true}));
+app.use(express.json());
+
+
+app.get('/',(req,res)=>res.render('index.ejs'));
 
 app.listen('2001',()=>console.log('server started!'));
